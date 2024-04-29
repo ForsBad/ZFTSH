fca9a9a106119f6477239fb536983f66a8014d13
ForsBad <148485141+ForsBad@users.noreply.github.com>
2024-04-29 15:51:01 +0300

    Update Task3.py

--- Manual/6/Task3.py
+++ Manual/6/Task3.py
@@ -1,12 +1,27 @@
+pd= []
+vd = []
+
+for i in range(3):
+    pd.append(int(input()))
+
+pst = int(input())
+pend = int(input())  
+
+for i in range(3):
+    vd.append(int(input()))
+
+vst = int(input())
+vend = int(input())
+
 def Work(pd,vd,pst,pend,vst,vend,Value = 0):    
     if(abs(pst-vst)>1):
-        print("0")
-        quit
+        return 0
     if(abs(pend-vend)>1):
-        print("0")
-        quit
+        return 0
     for i in range(3):
         for n in range(3):
             if (pd[i]==vd[n]):
                 Value += 1   
     return Value
+
+print(Work(pd,vd,pst,pend,vst,vend))
